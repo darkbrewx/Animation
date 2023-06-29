bb1ca57f7985e02d54fdce558e7098ff4201eb51
WENBO HU <darkbrewx@gmail.com>
2023-06-30 00:51:07 +0900

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 胡文博 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
